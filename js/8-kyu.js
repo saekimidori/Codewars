f458673384c93e7873e91b6c3fad1a6ff6aaf4f2
Carol Choi <saekimidori@gmail.com>
2022-04-14 22:15:30 -0400

    adding all current solutions

---
+++ js/8-kyu.js
@@ -0,0 +1,13 @@
+// How good are you really?
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+  //   let classPoints = []
+    for (i = 0; i <= classPoints.length; i++) {
+      let sum += i
+      console.log(sum)
+    }
+  //   return sum + yourPoints
+  
+  }
+
+  betterThanAverage([2, 3], 5)
