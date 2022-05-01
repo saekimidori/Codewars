6406e72ac1262b24b22610fcae736ac21e76c84f
Carol Choi <saekimidori@gmail.com>
2022-05-01 16:00:00 +0100

    Find Maximum and Minimum Values of a List solved

---
+++ 8-kyu/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,9 @@
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+function min(list) {
+  return Math.min(...list)
+}
+
+function max(list) {
+  return Math.max(...list)
+}
