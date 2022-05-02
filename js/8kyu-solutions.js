bb67bc744c43a341eced85c7d90f3f4248ff7a38
Carol Choi <saekimidori@gmail.com>
2022-05-01 21:32:50 -0400

    solved Find Maximum and Minimum Values of a List kata

--- js/8kyu-solutions.js
+++ js/8kyu-solutions.js
@@ -1,3 +1,14 @@
+// Find Maximum and Minimum Values of a List
+function min(list) {
+  return Math.min(...list)
+}
+
+function max(list) {
+    return Math.max(...list)
+}
+
+
+
 // Stringy Strings
 function stringy(size) {
   let str = ''
