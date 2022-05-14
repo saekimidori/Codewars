42d9e563a3a16ce6fd23b777c225daeee068503d
Carol Choi <saekimidori@gmail.com>
2022-05-13 23:27:32 -0400

    added own file

---
+++ 8-kyu/Beginner - Lost Without a Map.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    //P: take in an array of integers (x)
+    //R: a new array with each value doubled
+    //E:
+    //P: 
+    let doubled = x.map(x => x*2)
+    return doubled    
+}
