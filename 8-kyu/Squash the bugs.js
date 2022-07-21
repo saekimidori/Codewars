57650ca0afdeb0f167086e4706063e6138f93b92
Carol Choi <saekimidori@gmail.com>
2022-07-21 12:37:43 -0400

    Squash the bugs solved

---
+++ 8-kyu/Squash the bugs.js
@@ -0,0 +1,16 @@
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// There will only be one 'longest' word.
+
+const findLongest = str => {
+  
+    const spl = str.split(' ')
+    let longest = 0
+    
+    for (i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length    
+      }
+    }
+    return longest
+}
