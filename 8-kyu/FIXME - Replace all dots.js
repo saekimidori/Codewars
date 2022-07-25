f164ae3e9bbe61a4760fd7b380464ddae6503aa1
Carol Choi <saekimidori@gmail.com>
2022-07-25 18:56:34 -0400

    FIXME - Replace all dots solved

---
+++ 8-kyu/FIXME - Replace all dots.js
@@ -0,0 +1,7 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Fix the bug so we can all go home early.
+
+const replaceDots = str => str.split('.').join('-')
