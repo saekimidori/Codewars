bc37ce190298b5265b69c64a6e77338fbb7dbc47
Carol Choi <saekimidori@gmail.com>
2022-08-04 22:20:29 -0400

    Alan Partridge II - Apple Turnover solved

---
+++ 8-kyu/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,4 @@
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+const apple = x =>
+  x*x > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
