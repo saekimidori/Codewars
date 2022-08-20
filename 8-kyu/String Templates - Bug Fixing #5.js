342e159bacd7671a96f628c8ce1ded236b78e7ba
Carol Choi <saekimidori@gmail.com>
2022-08-20 12:17:47 -0400

    String Templates - Bug Fixing #5 solved

---
+++ 8-kyu/String Templates - Bug Fixing #5.js
@@ -0,0 +1,3 @@
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+const buildString = (...template) => `I like ${template.join(', ')}!`
