5fa77586d58564fb9a67eacb74e550a859fd49c7
Carol Choi <saekimidori@gmail.com>
2022-08-23 21:42:26 -0400

    Formatting decimal places #0 solved

---
+++ 8-kyu/Formatting decimal places #0.js
@@ -0,0 +1,3 @@
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+const twoDecimalPlaces = n => +n.toFixed(2)
