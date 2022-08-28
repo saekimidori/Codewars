f5aa93073ab5289450d7408fa4ad50e6b2b42b4d
Carol Choi <saekimidori@gmail.com>
2022-08-28 16:39:55 -0400

    Template Strings solved

---
+++ 8-kyu/Template Strings.js
@@ -0,0 +1,5 @@
+// Your task is to return the correct string using the Template String Feature.
+
+// You must output a string containing the two strings with the word ```' are '```
+
+const TempleStrings = (obj, feature) => `${obj} are ${feature}`
