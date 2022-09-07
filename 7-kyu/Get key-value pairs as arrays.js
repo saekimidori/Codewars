6819ac8e517d5309040236c17a8a9b0a72d5cd13
Carol Choi <saekimidori@gmail.com>
2022-09-06 22:47:38 -0400

    Get key-value pairs as arrays solved

---
+++ 7-kyu/Get key-value pairs as arrays.js
@@ -0,0 +1,3 @@
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+const keysAndValues = data => [Object.keys(data), Object.values(data)]
