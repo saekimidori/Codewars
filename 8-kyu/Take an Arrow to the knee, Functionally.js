ff5349d7680c215275c1ca9d7c12fa16ffd3a403
Carol Choi <saekimidori@gmail.com>
2022-09-24 17:44:02 -0400

    Take an Arrow to the knee, Functionally solved

---
+++ 8-kyu/Take an Arrow to the knee, Functionally.js
@@ -0,0 +1,3 @@
+// You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+const ArrowFunc = arr => arr.map(code => String.fromCharCode(code)).join('')
