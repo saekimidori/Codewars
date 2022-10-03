08563b1c1a31839d9ba315b310d258ad14929791
Carol Choi <saekimidori@gmail.com>
2022-10-02 20:23:07 -0400

    Contamination #1 -String-.js

---
+++ 8-kyu/Contamination #1 -String-.js
@@ -0,0 +1,12 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+const contamination = (text, char) =>
+  text.length === 0 || char.length === 0 ? '' :
+    [...text].map(el => char).join('')
