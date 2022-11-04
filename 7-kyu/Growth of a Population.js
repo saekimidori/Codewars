ee075dccd2eaa85b63a13601662829b68ea26ee9
Carol Choi <saekimidori@gmail.com>
2022-11-03 23:05:48 -0400

    Growth of a Population solved

---
+++ 7-kyu/Growth of a Population.js
@@ -0,0 +1,51 @@
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// At the end of the first year there will be: 
+// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+// At the end of the 2nd year there will be: 
+// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+// At the end of the 3rd year there will be:
+// 1141 + 1141 * 0.02 + 50 => 1213
+
+// It will need 3 entire years.
+
+// More generally given parameters:
+
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+// Examples:
+// nb_year(1500, 5, 100, 5000) -> 15
+// nb_year(1500000, 2.5, 10000, 2000000) -> 10
+
+// Note:
+// Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+const nbYear = (p0, percent, aug, p) => {
+    // receives 4 positive integers:
+    // p0: population at beginning of year
+    // percent: increase per year + 50
+    // aug: inhabitants coming/leaving each year
+    // p: population to equal or surpass
+    // return num of years needed to get populatation >= p
+    
+    // define variables => total pop, years
+    let totalPop = p0
+    let years = 0
+    // calculate pop => convert % => percent/100
+    // total pop + (current pop * percent) + aug
+    const calcPop = currentPop => currentPop + (currentPop * percent/100) + aug
+    // while total pop is < p, keep calculating
+    while (totalPop < p) {
+      // ensure pop is whole number
+      totalPop = Math.floor(calcPop(totalPop, percent, aug))
+      years += 1
+    }
+    // current pop >= p
+    return years
+}
