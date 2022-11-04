d5aecbfbd87b98c27b19bc5a5ed63e22cc930ae3
Carol Choi <saekimidori@gmail.com>
2022-11-04 15:34:44 -0400

    String ends with solved

---
+++ 7-kyu/String ends with.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+const solution = (str, ending) => 
+  // takes in two strings
+  // return boolean if 1st str ends with 2nd str
+  
+  // endsWith
+  str.endsWith(ending)
