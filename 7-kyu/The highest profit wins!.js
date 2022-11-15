87b574e82ec13a0a7b7c422112a36d56ac0f7a32
Carol Choi <saekimidori@gmail.com>
2022-11-14 21:10:55 -0500

    The highest profit wins! solved

---
+++ 7-kyu/The highest profit wins!.js
@@ -0,0 +1,10 @@
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+const minMax = arr =>
+  // takes an array
+  // return min and max num in an array
+  
+  // Math.min/max(...arr)
+  [Math.min(...arr), Math.max(...arr)]
