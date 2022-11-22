3118c891c3e5a10a98b08cb9bfbfa301d6185084
Carol Choi <saekimidori@gmail.com>
2022-11-22 15:27:20 -0500

    Make a function that does arithmetic! solved

---
+++ 7-kyu/Make a function that does arithmetic!.js
@@ -0,0 +1,29 @@
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// Try to do it without using if statements!
+
+const arithmetic = (a, b, operator) => {
+    // takes two positive integers and an operator as string
+    // return result of nums w/ operator used on nums
+    // first always a, second always b
+    
+    // switch statement
+    switch (operator) {
+        case 'add':
+          return a + b
+        break
+        case 'subtract':
+          return a - b
+        break
+        case 'multiply':
+          return a * b
+        break
+        case 'divide':
+          return a / b
+        break
+    }
+}
